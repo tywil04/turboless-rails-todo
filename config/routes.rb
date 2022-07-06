3242b287cc247b72f11ee9e74dfebec5af34b8f3
Tyler <tylerwilliams@Tylers-MacBook-Pro.local>
2022-07-06 20:12:01 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
+  resources :todos
   root "todos#index"
 end
